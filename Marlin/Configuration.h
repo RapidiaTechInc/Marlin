058b066d4cf29badc0a65575e004972da1b3195a
nb <noahb@rapidia.com>
2020-05-04 13:59:32 -0700

    enabled SD card

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -26,6 +26,9 @@
 
 #define RAPIDIA_PROTOCOL 2
 
+// #define RAPIDIA_NO_HOTENDS
+// #define RAPIDIA_NO_EXTRUDE
+
 #ifndef RAPIDIA_PLASTIC
     #define RAPIDIA_METAL
 #endif
@@ -1705,7 +1708,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
