5defd7b27a8be7a80352246cd90f9b80d7e93e67
nb <noahb@rapidia.com>
2020-07-16 13:04:50 -0700

    [V2 backport] Updated probe offset for RAPIDIA_PLASTIC

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -800,9 +800,9 @@
  */
 
 #ifdef RAPIDIA_PLASTIC
-#  define X_PROBE_OFFSET_FROM_EXTRUDER 20  // X offset: -left  +right  [of the nozzle]
-#  define Y_PROBE_OFFSET_FROM_EXTRUDER 24  // Y offset: -front +behind [the nozzle]
-#  define Z_PROBE_OFFSET_FROM_EXTRUDER 2   // Z offset: -below +above  [the nozzle]
+#  define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
+#  define Y_PROBE_OFFSET_FROM_EXTRUDER 10  // Y offset: -front +behind [the nozzle]
+#  define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 #endif
 
 #ifdef RAPIDIA_METAL
