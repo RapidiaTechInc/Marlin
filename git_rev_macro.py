ac1825090bb9bae85e60f7c00b33ce84b44e8611
nb <noahb@rapidia.com>
2020-07-16 12:51:08 -0700

    Updated capabality report (M115) to be more consistent
    Summary: RAPIDIA:FIRMWARE_VERSION field added. Python rev parse script updated to remove erroneous bytes marker (b'') on some versions.
    
    Test Plan: Run firmware, view m115 message.
    
    Reviewers: tlai, edwinc
    
    Reviewed By: tlai
    
    Differential Revision: https://phabricatorserver.rapidia.com/D138

--- git_rev_macro.py
+++ git_rev_macro.py
@@ -3,4 +3,13 @@
 import subprocess
 
 revision = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+
+# account for python version differences changing how this string is produced.
+revision = str(revision);
+if revision.startswith("b'"):
+    revision = revision[2:]
+if revision.endswith("'"):
+    revision = revision[:-1]
+
+# stdout is read by platformio.
 print("-DRAPIDIA_SRC_REV=\"%s\"" % revision)
