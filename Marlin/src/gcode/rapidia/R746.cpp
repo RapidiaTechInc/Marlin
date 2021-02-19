5abb223b265feafe7f97a9fc71a6fe639ccab251
nb <noahb@rapidia.com>
2021-02-19 14:10:15 -0800

    cleaned up R746 code

--- Marlin/src/gcode/rapidia/R746.cpp
+++ Marlin/src/gcode/rapidia/R746.cpp
@@ -40,133 +40,58 @@
 #define DEBUG_OUT ENABLED(DEBUG_LEVELING_FEATURE)
 #include "../../core/debug_out.h"
 
-void do_t1_homing_move(const AxisEnum axis, const float distance, const feedRate_t fr_mm_s=0.0) {
-  DEBUG_SECTION(log_move, "do_t1_homing_move", DEBUGGING(LEVELING));
+#if ENABLED(RAPIDIA_T1_HOMING)
 
-  const feedRate_t real_fr_mm_s = fr_mm_s ?: homing_feedrate(axis);
-
-  // Only do some things when moving towards an endstop
-  const int8_t axis_home_dir = TERN0(DUAL_X_CARRIAGE, axis == X_AXIS)
-                ? x_home_dir(active_extruder) : home_dir(axis);
-  const bool is_moving_towards_endstop = (axis_home_dir > 0) == (distance > 0);
-
-  if (is_moving_towards_endstop) {
-    #if HOMING_Z_WITH_PROBE && QUIET_PROBING
-      if (axis == Z_AXIS) probe.set_probing_paused(true);
-    #endif
-  }
-
-  // Get the ABC or XYZ positions in mm
-  sync_plan_position();
-  abce_pos_t target = planner.get_axis_positions_mm();
-
-  #if HAS_DIST_MM_ARG
-    const xyze_float_t cart_dist_mm{0};
-  #endif
-
-  // Set delta/cartesian axes directly
-  target[axis] += distance;                  // The move will be towards the endstop
-  planner.buffer_segment(target
-    #if HAS_DIST_MM_ARG
-      , cart_dist_mm
-    #endif
-    , real_fr_mm_s, active_extruder
-  );
-
-  planner.synchronize(); // wait until z hits endstop
-
-  if (is_moving_towards_endstop) {
-
-    #if HOMING_Z_WITH_PROBE && QUIET_PROBING
-      if (axis == Z_AXIS) probe.set_probing_paused(false);
-    #endif
-
-    endstops.validate_homing_move();
-  }
+static void park_toolhead(uint8_t toolhead=active_extruder) {
+  uint8_t prev = active_extruder;
+  tool_change(toolhead, true);
+  do_blocking_move_to_x(x_home_pos(toolhead));
+  tool_change(prev, true);
 }
 
-void homeT1Z() { // in T0's coordinates
-  const int axis_home_dir = home_dir(Z_AXIS);
+static void t1_probe()
+{
+  tool_change(1, true);
 
-  // Homing Z towards the bed? Deploy the Z probe or endstop.
-  if (TERN0(HOMING_Z_WITH_PROBE, probe.deploy()))
-    return;
+  hotend_offset[1].z = probe.probe_at_point(safe_homing_xy);
 
-  do_t1_homing_move(Z_AXIS, 1.5f * max_length(Z_AXIS) * axis_home_dir);
+  // for z clearance and raising
+  tool_change(0, true);
 
-  // When homing Z with probe respect probe clearance
-  const float bump = axis_home_dir * home_bump_mm(Z_AXIS);
-
-  // If a second homing move is configured...
-  if (bump) {
-    // Move away from the endstop by the axis HOMING_BUMP_MM
-    do_t1_homing_move(Z_AXIS, -bump
-      #if HOMING_Z_WITH_PROBE
-        , MMM_TO_MMS(Z_PROBE_SPEED_FAST)
-      #endif
-    );
-
-    // Slow move towards endstop until triggered
-    do_t1_homing_move(Z_AXIS, 2 * bump, get_homing_bump_feedrate(Z_AXIS));
-  }
-
-  // Set T1 Z Offset
-  sync_plan_position();
-  hotend_offset[1].z = stepper.report_state().position[Z_AXIS] / planner.settings.axis_steps_per_mm[Z_AXIS]; // Current position of T0 Z
-  SERIAL_ECHO_START();
-  SERIAL_ECHOPGM("Set T1 Z Offset to: ");
-  SERIAL_ECHO_F(hotend_offset[1].z, 3);
-  SERIAL_EOL();
-
-  // Put away the Z probe
-  #if HOMING_Z_WITH_PROBE
-    if (probe.stow()) return;
-  #endif
-
-} // homeaxis()
-
-static void home_t1_z_safely() {
-  /**
-   * Move the Z probe (or just the nozzle) to the safe homing point
-   * (Z is already at the right height)
-   * Set destination to center of bed with same height
-   */
-
-  tool_change(1, true); // change to t1 without moving
-
-  destination.set(safe_homing_xy, current_position.z);
-
-  TERN_(HOMING_Z_WITH_PROBE, destination -= probe.offset_xy);
-
-  if (position_is_reachable(destination)) {
-    // This causes the carriage on Dual X to unpark
-    TERN_(DUAL_X_CARRIAGE, active_extruder_parked = false);
-    do_blocking_move_to_xy(destination);
-    tool_change(0, true); // change to t1 without moving
-    homeT1Z();
-  }
-  else {
-    SERIAL_ECHO_MSG(STR_ZPROBE_OUT_SER);
-  }
+  // probe.move_z_after_homing(); // raise after home
+  do_z_clearance(Z_AFTER_HOMING, true, true, false);
 }
 
 void GcodeSuite::R746() {
+
+  if (ENABLED(RAPIDIA_PLASTIC))
+  {
+    SERIAL_ECHO_MSG("R746 not supported on plastic printers");
+    return;
+  }
+
   // Check T0 already homed
   if (homing_needed_error()) {
     return;
   }
+
   // Check dualx mode was not set to something weird
+  if (dual_x_carriage_mode != DXC_FULL_CONTROL_MODE) {
+    SERIAL_ERROR_MSG("dualx mode must be set to full control mode. (M605 S0)");
+    return;
+  }
+
+  uint8_t prev_extruder = active_extruder;
 
   RAISE_HOMING_SEMAPHORE();
 
-  // Wait for planner moves to finish!
+  // Wait for planner moves to finish.
   planner.synchronize();
 
   hotend_offset[1].z = 0; // reset t1 z offset to 0 before moving
 
   // Disable the leveling matrix before homing (REVISIT THIS LATER)
   #if HAS_LEVELING
-
     // Cancel the active G29 session
     TERN_(PROBE_MANUALLY, g29_in_progress = false);
 
@@ -181,53 +106,22 @@ void GcodeSuite::R746() {
 
   remember_feedrate_scaling_off();
 
-  endstops.enable(true); // Enable endstops for next homing move
+  endstops.enable(true); // Enable endstops upcoming homing moves
 
-  tool_change(0, true); // change to t0 without moving in order to dock it
+  park_toolhead(0); // always dock t0 to prevent crashing
 
-  // TODO: Don't home but just park it
-  homeaxis(X_AXIS); // always dock t0 to prevent crashing
-
-  tool_change(1, true); // change to t0 without moving in order to dock it
-
-  const float z_homing_height =
-    ENABLED(UNKNOWN_Z_NO_RAISE) && !TEST(axis_known_position, Z_AXIS)
-      ? 0
-      : (parser.seenval('R') ? parser.value_linear_units() : Z_HOMING_HEIGHT);
-
-  if (z_homing_height) {
-    // Raise Z before homing any other axes and z is not already high enough (never lower z)
-    if (DEBUGGING(LEVELING)) DEBUG_ECHOLNPAIR("Raise Z (before homing) by ", z_homing_height);
-    do_z_clearance(z_homing_height, true, DISABLED(UNKNOWN_Z_NO_RAISE));
-  }
-
-  // Home Z
   TERN_(BLTOUCH, bltouch.init());
 
-  // home_t1_z_safely();
-  hotend_offset[1].z = probe.probe_at_point(safe_homing_xy);
-  SERIAL_ECHO_START();
-  SERIAL_ECHOPGM("Set T1 Z Offset to: ");
-  SERIAL_ECHO_F(hotend_offset[1].z, 3);
-  SERIAL_EOL();
+  // main homing logic for T1.
+  t1_probe();
 
-  tool_change(0, true); // change to t0 without moving in order to dock it
-
-
-  // probe.move_z_after_homing(); // raise after home
-  do_z_clearance(Z_AFTER_HOMING, true, true, false);
-
-  SERIAL_ECHOLNPGM("Finished homing T1 Z.");
-
+  // restore previous settings
   endstops.not_homing();
-
   TERN_(RESTORE_LEVELING_AFTER_G28, set_bed_leveling_enabled(leveling_was_active));
-
+  tool_change(prev_extruder, true);
   restore_feedrate_and_scaling();
-
-  #if defined(Z_AFTER_HOMING)
-    do_blocking_move_to_z(Z_AFTER_HOMING);
-  #endif
 
   report_current_position();
 }
+
+#endif
